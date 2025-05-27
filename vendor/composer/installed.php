cd74e923a075fa7f7592fa6d21645a3a86091c69
OS-03 <ferozshaikh2222@gmail.com>
2025-05-27 11:40:26 +0530

    final project commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'owaisshaikh/pg-life',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b0ce938a4367865ea7b3a6db3a44cccecbc4f49e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'owaisshaikh/pg-life' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b0ce938a4367865ea7b3a6db3a44cccecbc4f49e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
